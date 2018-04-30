2d75db987917d1fab1809ffeb98ecfd80885a847
Man Le <man.lekhac@lazada.com>
2018-04-30 23:51:55 +0700

    Update todoList with filters

--- __tests__/containers/VisibleTodoList.js
+++ __tests__/containers/VisibleTodoList.js
@@ -6,16 +6,19 @@ import {
 describe("VisibleTodoListContainer", () => {
     const cases = [
         {
-            todos: {
-                1: {
-                    id: 1,
-                    content: "todo 1",
-                    active: true
-                },
-                2: {
-                    id: 2,
-                    content: "todo 2",
-                    active: false
+            state: {
+                filter: "",
+                todos: {
+                    1: {
+                        id: 1,
+                        content: "todo 1",
+                        active: true
+                    },
+                    2: {
+                        id: 2,
+                        content: "todo 2",
+                        active: false
+                    }
                 }
             },
             expected: [
@@ -30,18 +33,66 @@ describe("VisibleTodoListContainer", () => {
                     active: false
                 }
             ]
+        },
+        {
+            state: {
+                filter: "SHOW_ACTIVE",
+                todos: {
+                    1: {
+                        id: 1,
+                        content: "todo 1",
+                        active: true
+                    },
+                    2: {
+                        id: 2,
+                        content: "todo 2",
+                        active: false
+                    }
+                }
+            },
+            expected: [
+                {
+                    id: 1,
+                    content: "todo 1",
+                    active: true
+                }
+            ]
+        },
+        {
+            state: {
+                filter: "SHOW_COMPLETED",
+                todos: {
+                    1: {
+                        id: 1,
+                        content: "todo 1",
+                        active: true
+                    },
+                    2: {
+                        id: 2,
+                        content: "todo 2",
+                        active: false
+                    }
+                }
+            },
+            expected: [
+                {
+                    id: 2,
+                    content: "todo 2",
+                    active: false
+                }
+            ]
         }
     ];
 
     test("getTodolListFrom", () => {
         cases.forEach(c => {
-            expect(getTodoListFrom({ todos: c.todos })).toEqual(c.expected);
+            expect(getTodoListFrom(c.state)).toEqual(c.expected);
         });
     });
 
     test("mapStateToProps", () => {
         cases.forEach(c => {
-            expect(mapStateToProps({ todos: c.todos })).toEqual({
+            expect(mapStateToProps(c.state)).toEqual({
                 todos: c.expected
             });
         });
--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,8 +1,17 @@
 import { connect } from "react-redux";
 import { TodoList } from "../components/TodoList";
+import { FILTER } from "../flux/filter";
 
 export function getTodoListFrom(state) {
-    return Object.values(state.todos);
+    const fullList = Object.values(state.todos);
+    switch (state.filter) {
+        case FILTER.SHOW_ACTIVE:
+            return fullList.filter(todo => todo.active);
+        case FILTER.SHOW_COMPLETED:
+            return fullList.filter(todo => !todo.active);
+        default:
+            return fullList;
+    }
 }
 
 export function mapStateToProps(state) {
