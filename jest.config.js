4a4dac70badb50b0dd9d26e39eda41420aeac957
Man Le <man.lekhac@lazada.com>
2018-04-30 21:44:20 +0700

    Fix eslint - prettier integration

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     setupTestFrameworkScriptFile: "./node_modules/jest-enzyme/lib/index.js",
-    testPathIgnorePatterns: [".config.js"],
+    testPathIgnorePatterns: [".config.js", "__mocks__"],
     // testMatch: [],
     setupFiles: ["./__tests__/test.config.js"],
+    // roots: ["<rootDir>", "<rootDir>/__tests__"],
     unmockedModulePathPatterns: ["react", "enzyme", "jest-enzyme"]
 };
